1633e9f3abfc1629bc5f119e95e0599e861a7b2d
Dan Chianucci <dan.chianucci@gmail.com>
2022-06-14 16:04:06 +0200

    Made svn env info commands better

--- configs/bash/bashrc.d/envinfo_funcs.sh
+++ configs/bash/bashrc.d/envinfo_funcs.sh
@@ -2,43 +2,37 @@
 
 # Returns (svn  <branch> <rev>) if applicable
 function svn_branch_info() {
-    local branch dirty rev info
-    if info=$(svn info 2>/dev/null); then
-        branch=$(svn_parse_branch "${info}")
+    local branch rev url
 
-        # Uncomment if you want to display the current revision.
-        rev=$(echo "${info}" | awk '/^Revision: [0-9]+/{print $2}')
-
-        # Uncomment if you want to display whether the repo is 'dirty.' In some
-        # cases (on large repos) this may take a few seconds, which can
-        # noticeably delay your prompt after a command executes.
-        # [ "$(svn status)" ] && dirty='*'
+    url="$(svn info --show-item url 2>/dev/null)"
+    if [ "$url" != "" ] ; then
+        rev="$(svn info --show-item revision)"
+        branch=$(svn_parse_branch "${url}")
 
         if [ "$branch" != "" ] ; then
-            echo "svn ${branch}${dirty} r${rev}"
+            echo "svn ${branch} r${rev}"
         fi
-        return 0
     fi
-    return 1
 }
 
 # Returns the current branch or tag name from the given `svn info` output
 function svn_parse_branch() {
     local chunk url
 
-    if ! url=$(echo "$1" | awk '/^URL: .*/{print $2}')  ; then
-        echo "trunk" && return
+    if ! url="$1"; then
+        echo "???" && return
     else
         declare -A match_types
         match_types["tg"]="tags"
         match_types["rl"]="releases"
-        match_types["us"]="branches\/user"
-        match_types["fe"]="branches\/feature"
+        match_types["us"]="branches\/user\(_branches\)\?"
+        match_types["pr"]="branches\/project\(_branches\)\?"
+        match_types["fe"]="branches\/feature\(_branches\)\?"
         match_types["br"]="branches"
 
-        for matchtype in tg rl us fe br; do
+        for matchtype in tg rl us pr fe br; do
             ptrn=${match_types["$matchtype"]}
-            chunk="$(echo "${url}/" | sed -n "s/.*\b${ptrn}\b//p" | grep -E -o '^[^/]+')"
+            chunk="$(echo "${url}/" | sed -n "s/.*\b${ptrn}\/\b//p" | grep -E -o '^[^/]+')"
             [ -n "$chunk" ] && echo "${matchtype}:${chunk}" && return 3
         done
 
